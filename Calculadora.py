8274799f339330bc627653a3d7be7bbff6bac58b
Vina del Mar <vina@duoc.acad>
2025-06-09 16:18:04 -0400

    Agregue una resta

--- Calculadora.py
+++ Calculadora.py
@@ -3,3 +3,7 @@ print("Calculadora")
 v1 = int(input("Valor 1 "))
 v2 = int(input("Valor 2 "))
 print("EL resultado es", v1+v2)
+
+v1 = int(input("Valor 1 "))
+v2 = int(input("Valor 2 "))
+print("EL resultado es", v1-v2)
